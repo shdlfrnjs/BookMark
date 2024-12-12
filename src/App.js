bd7ade84ce5af260b68ae362355aaecb3c0d12e2
seungjuncha <seungjuncha506@gmail.com>
2024-12-12 19:25:21 +0900

    탭 바 아이콘 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AsyncStorage from "@react-native-async-storage/async-storage"; // AsyncStorage 임포트
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from "./screens/HomeScreen";
 import DetailsScreen from "./screens/DetailsScreen";
 import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
-import SettingsScreen from "./screens/SettingsScreen"; // 설정 화면 추가
+import SettingsScreen from "./screens/SettingsScreen";
 import MyBooksScreen from "./screens/MyBooksScreen";
 import MyReadingLogs from "./screens/MyReadingLogs";
 import ReadingScheduleScreen from "./screens/ReadingScheduleScreen";
@@ -37,7 +38,35 @@ const App = () => {
   }, []);
 
   const TabNavigator = () => (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: { backgroundColor: '#fff' },
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+  
+          switch (route.name) {
+            case "도서 검색":
+              iconName = "search-outline";
+              break;
+            case "독서 일정":
+              iconName = "calendar-outline";
+              break;
+            case "나의 도서":
+              iconName = "book-outline";
+              break;
+            case "마이페이지":
+              iconName = "person-outline"; 
+              break;
+            default:
+              iconName = "ellipse-outline";
+          }
+  
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
       <Tab.Screen name="도서 검색" component={HomeScreen} />
       <Tab.Screen name="독서 일정" component={ReadingScheduleScreen} />
       <Tab.Screen name="나의 도서" component={MyBooksScreen} />
